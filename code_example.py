18c503272d8789f3ff407cc6062c27a4aca334f4
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2025-11-03 02:17:59 +0900

    edit day2 pyxel部分を追加

--- code_example.py
+++ code_example.py
@@ -1,5 +1,27 @@
-def square(hen):
-  menseki = hen ** 2
-  return menseki
+import pyxel
 
-print(square(5))
+# pyxelを初期化：画面のサイズを２００ｘ２００ピクセルに設定
+pyxel.init(200, 200)
+
+# 四角に関する情報を定義
+rect_info = {
+    "x": 10,
+    "y": 10,
+    "w": 100,
+    "h": 100,
+    "col": 7
+}
+
+# update関数：四角を移動する処理をする
+def update():
+    rect_info["x"] = rect_info["x"] + 1
+
+# draw関数：四角を描画する
+def draw():
+    # 背景色を黒にする
+    pyxel.cls(0)
+    # 四角を描画
+    pyxel.rect(rect_info["x"], rect_info["y"], rect_info["w"], rect_info["h"], rect_info["col"])
+
+pyxel.run(update, draw)
+
