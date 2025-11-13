e70b39b2a146b9f9acc11938f602a40139b1b701
axcelcamp_ricky <axcelcamp.teacher@gmail.com>
2025-11-13 22:54:06 +0900

    edit day4: アニメーション

--- danmaku.py
+++ danmaku.py
@@ -2,9 +2,21 @@ import pyxel
 pyxel.init(120, 120)
 pyxel.load("danmaku.pyxres")
 
-def update():
-    pass
-def draw():
-    pyxel.bltm()
-pyxel.run(update, draw)
+character = {
+    "x": 120 / 2 - 16 / 2,
+    "y": 90
+}
+enemy = {
+    "x": 120 / 2 - 16 / 2,
+    "y": 20
+}
 
+def update():
+    character["x"] = character["x"] + 1
+
+def draw():
+    pyxel.cls(0)
+    pyxel.blt(character["x"], character["y"], 0, 0, 0, 16, 16, 0)
+    pyxel.blt(enemy["x"], enemy["y"], 0, 16, 0, 16, 16, 0)
+
+pyxel.run(update, draw)
